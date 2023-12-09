a9bf3a7862942aec38fbbc2321a116de2303e46e
dbswp <dbswp980427@gmail.com>
2023-12-10 00:45:18 +0900

    jwt 토큰 생성

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 // require('./controllers/mongoConnect'); -> mongo connect 확인
@@ -16,6 +17,7 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Router
 const testRouter = require("./routes/test");
--- controllers/userController.js
+++ controllers/userController.js
@@ -101,97 +101,6 @@ const getMyPage = async (req, res) => {
   } catch {}
 };
 
-// const joinPersonal = async (req, res) => {
-//   try {
-//     const { data } = req.body;
-//     const name = data.user_name;
-//     const phone = data.user_tel;
-//     const id = data.user_id;
-//     const password = data.user_password;
-//     const type = data.user_type;
-
-//     console.log(type);
-//     console.log(name);
-//     console.log(phone);
-//     console.log(id);
-//     console.log(password);
-
-//     // 빈값이 오면 팅겨내기
-//     if (name === "" || phone === "") {
-//       return res.json({ registerSuccess: false, message: "정보를 입력하세요" });
-//     }
-//     const samePhoneUser = await User.findOne({ phone });
-//     if (samePhoneUser !== null) {
-//       return res.json({
-//         registerSuccess: false,
-//         message: "이미 존재하는 번호입니다.",
-//       });
-//     }
-//     const sameIdUser = await User.findOne({ id });
-//     if (sameIdUser !== null) {
-//       return res.json({
-//         registerSuccess: false,
-//         message: "이미 존재하는 아이디입니다",
-//       });
-//     }
-//     // 빈값이 오면 팅겨내기
-//     if (id === "" || password === "" || passwordCheck === "") {
-//       return res.json({ registerSuccess: false, message: "정보를 입력하세요" });
-//     }
-
-//     // 솔트 생성 및 해쉬화 진행
-//     const salt = await bcrypt.genSalt(saltRounds);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-
-//     const user = await User.insertMany({
-//       name,
-//       phone,
-//       id,
-//       type,
-//       password: hashedPassword,
-//     });
-//     if (!user) return res.status(400).json("실패");
-//     return res.json({ registerSuccess: true });
-//   } catch (error) {
-//     return res.json({ registerSuccess: false, message: error.message });
-//   }
-// };
-
-// const joinMember = async (req, res) => {
-//   try {
-//     const { data } = req.body;
-//     const id = data.user_id;
-//     const password = data.user_pw;
-
-//     // 빈값이 오면 팅겨내기
-//     if (id === "" || password === "" || passwordCheck === "") {
-//       return res.json({ registerSuccess: false, message: "정보를 입력하세요" });
-//     }
-//     const sameIdUser = await User.findOne({ id });
-//     if (sameIdUser !== null) {
-//       return res.json({
-//         registerSuccess: false,
-//         message: "이미 존재하는 아이디입니다",
-//       });
-//     }
-//     // 솔트 생성 및 해쉬화 진행
-//     const salt = await bcrypt.genSalt(saltRounds);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-//     const user = new User({
-//       id,
-//       password: hashedPassword,
-//     });
-//     await user.save();
-//     res.send("저장 성공");
-//     console.log(id);
-//     console.log(password);
-//     console.log(hashedPassword);
-//     return res.json({ registerSuccess: true });
-//   } catch (error) {
-//     return res.json({ registerSuccess: false, message: error.message });
-//   }
-// };
-
 //로그인 미들웨어
 const loginUser = async (req, res) => {
   const { id, pw } = req.body;
@@ -219,15 +128,25 @@ const loginUser = async (req, res) => {
     }
     console.log(pw, user.user_pw);
 
-    const token = jwt.sign({ type: "jwt", id: user.user_id }, ACCESS_SECRET, {
-      expiresIn: "7d",
-    });
+    const token = jwt.sign(
+      { type: "jwt", name: user.user_name },
+      ACCESS_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+    if (!token) {
+      return res.status(500).json({
+        loginSuccess: false,
+        message: "토큰 발행 중에 오류가 발생했습니다.",
+      });
+    }
     user.token = token;
     await user.save();
 
     return res
       .status(200)
-      .json({ loginSuccess: true, id: user.user_id, token });
+      .json({ loginSuccess: true, name: user.user_name, token: token });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "something wrong" });
